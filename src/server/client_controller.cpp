5467d5e740b609781d60d1e66bb7affc00b133a8
PaulDotSH <84643540+PaulDotSH@users.noreply.github.com>
2024-04-24 15:39:15 -0400

    make it working :)

--- src/server/client_controller.cpp
+++ src/server/client_controller.cpp
@@ -1,38 +1,32 @@
 #include "client_controller.hpp"
 #include "protocol.hpp"
+#include <sys/socket.h>
 
-std::function<void(std::shared_ptr<ConnectionBuffer>)> ClientController::handlers[UINT8_MAX + 1];
+std::function<void(std::shared_ptr<ConnectionBuffer>)>
+    ClientController::handlers[UINT8_MAX + 1];
 
-void ClientController::initialize()
-{
-    handlers[REQ_SERVER_STATUS] = [](std::shared_ptr<ConnectionBuffer> cb)
-        {};
+void ClientController::initialize() {
+  handlers[REQ_SERVER_STATUS] = [](std::shared_ptr<ConnectionBuffer> cb) {
+    printf("REQ_SERVER_STATUS\n");
+    send(cb->connection->socket_fd, ":)", 3, 0);
+  };
 
-    handlers[REQ_CRC_VERIFY] = [](std::shared_ptr<ConnectionBuffer> cb)
-        {};
+  handlers[REQ_CRC_VERIFY] = [](std::shared_ptr<ConnectionBuffer> cb) {};
 
-    handlers[REQ_FILES_TRANSFER_START] = [](std::shared_ptr<ConnectionBuffer> cb)
-        {};
+  handlers[REQ_FILES_TRANSFER_START] =
+      [](std::shared_ptr<ConnectionBuffer> cb) {};
 
-    handlers[REQ_FILE_TRANSFER_CHUNK] = [](std::shared_ptr<ConnectionBuffer> cb)
-        {};
+  handlers[REQ_FILE_TRANSFER_CHUNK] = [](std::shared_ptr<ConnectionBuffer> cb) {
+  };
 
-    handlers[REQ_FILE_TRANSFER_END] = [](std::shared_ptr<ConnectionBuffer> cb)
-        {};
+  handlers[REQ_FILE_TRANSFER_END] = [](std::shared_ptr<ConnectionBuffer> cb) {};
 
-    handlers[REQ_GET_SETTINGS] = [](std::shared_ptr<ConnectionBuffer> cb)
-        {};
+  handlers[REQ_GET_SETTINGS] = [](std::shared_ptr<ConnectionBuffer> cb) {};
 
-    handlers[REQ_SET_SETTING] = [](std::shared_ptr<ConnectionBuffer> cb)
-        {};
-
-
+  handlers[REQ_SET_SETTING] = [](std::shared_ptr<ConnectionBuffer> cb) {};
 }
 
-
-
-void ClientController::process_packet(std::shared_ptr<ConnectionBuffer> cb)
-{
-    auto packet_type = cb->get_packet_type();
-    handlers[packet_type](cb);
+void ClientController::process_packet(std::shared_ptr<ConnectionBuffer> cb) {
+  auto packet_type = cb->get_packet_type();
+  handlers[packet_type](cb);
 }
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,216 +1,205 @@
 #include <atomic>
 #include <cerrno>
-#include <cstdio>
-#include <cstring>
+#include <chrono>
 #include <csignal>
 #include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <memory>
 #include <mutex>
 #include <stack>
+#include <thread>
 #include <vector>
 
 #include <fcntl.h>
 #include <netinet/in.h>
 
+#include "client_controller.hpp"
 #include "protocol.hpp"
 #include "thread_pool.hpp"
-#include "client_controller.hpp"
 
-#define PORT    1312
+#define PORT 1312
 #define BACKLOG UINT16_MAX
 
-struct ServerState
-{
-    using TPendingConnections = std::stack<std::shared_ptr<ConnectionWrapper>, std::vector<std::shared_ptr<ConnectionWrapper>>>;
+struct ServerState {
+  using TPendingConnections =
+      std::stack<std::shared_ptr<ConnectionWrapper>,
+                 std::vector<std::shared_ptr<ConnectionWrapper>>>;
 
-    TPendingConnections pending_connections;
-    std::mutex          pending_connections_mtx;
+  TPendingConnections pending_connections;
+  std::mutex pending_connections_mtx;
 
-    std::vector<std::shared_ptr<ConnectionWrapper>> active_connections;
-    std::stack<std::shared_ptr<ConnectionBuffer>>   completed_packets;
-    ThreadPool                                      thread_pool;
+  std::vector<std::shared_ptr<ConnectionWrapper>> active_connections;
+  std::stack<std::shared_ptr<ConnectionBuffer>> completed_packets;
+  ThreadPool thread_pool;
 
-    std::atomic_bool b_should_run     = true;
-    std::atomic_int  server_socket_fd = -1;
+  std::atomic_bool b_should_run = true;
+  std::atomic_int server_socket_fd = -1;
 };
 
 static ServerState g_state;
 
-void make_socket_non_blocking(int socket_fd)
-{
-    int flags = fcntl(socket_fd, F_GETFL, 0);
-    if(fcntl(socket_fd, F_SETFL, flags | O_NONBLOCK) == -1)
-    {
-        //Handle error sometime
-    }
+void make_socket_non_blocking(int socket_fd) {
+  int flags = fcntl(socket_fd, F_GETFL, 0);
+  if (fcntl(socket_fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+    // Handle error sometime
+  }
 }
 
-void handle_packets()
-{
-    while(!g_state.completed_packets.empty())
-    {
-        auto& p = g_state.completed_packets.top();
+void handle_packets() {
+  while (!g_state.completed_packets.empty()) {
+    auto &p = g_state.completed_packets.top();
 
-        g_state.thread_pool.dispatch_async([_p = std::move(p)]()
-        {
-            ClientController::process_packet(std::move(_p));
-        });
+    g_state.thread_pool.dispatch_async([_p = std::move(p)]() {
+      ClientController::process_packet(std::move(_p));
+    });
 
-        g_state.completed_packets.pop();
-    }
+    g_state.completed_packets.pop();
+  }
 }
 
-void accept_thread_handler()
-{
-    int server_socket_fd;
-    if((server_socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
-    struct sockaddr_in address;
-    address.sin_family      = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port        = htons(PORT);
-    int address_length = sizeof(address);
-    if(bind(server_socket_fd, (struct sockaddr*)&address, address_length) < 0)
-    {
-        perror("bind");
-        exit(EXIT_FAILURE);
-    }
+void accept_thread_handler() {
+  int server_socket_fd;
+  if ((server_socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+  int tmp;
+  setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR, &tmp, sizeof(int));
+  struct sockaddr_in address;
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(PORT);
+  int address_length = sizeof(address);
+  if (bind(server_socket_fd, (struct sockaddr *)&address, address_length) < 0) {
+    perror("bind");
+    exit(EXIT_FAILURE);
+  }
 
-    if(listen(server_socket_fd, BACKLOG) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    g_state.server_socket_fd.exchange(server_socket_fd);
+  if (listen(server_socket_fd, BACKLOG) < 0) {
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
+  g_state.server_socket_fd.exchange(server_socket_fd);
 
-    while(true)
-    {
-        int client_socket_fd = accept(server_socket_fd, (struct sockaddr*)&address, (socklen_t*)&address_length);
-        if(client_socket_fd == -1)
-        {
-            break;
-        }
-        make_socket_non_blocking(client_socket_fd);
-        auto cw = std::make_shared<ConnectionWrapper>(client_socket_fd);
-        {
-            std::lock_guard guard{g_state.pending_connections_mtx};
-            g_state.pending_connections.push(std::move(cw));
-        }
+  while (true) {
+    int client_socket_fd = accept(server_socket_fd, (struct sockaddr *)&address,
+                                  (socklen_t *)&address_length);
+    if (client_socket_fd == -1) {
+      break;
     }
+    make_socket_non_blocking(client_socket_fd);
+    auto cw = std::make_shared<ConnectionWrapper>(client_socket_fd);
+    {
+      std::lock_guard guard{g_state.pending_connections_mtx};
+      g_state.pending_connections.push(std::move(cw));
+    }
+  }
 }
 
-void recieve_all_connections()
-{
-    for(auto it = g_state.active_connections.begin(); it != g_state.active_connections.end(); ++it)
-    {
-        auto& connection = *it;
+void recieve_all_connections() {
+  for (auto it = g_state.active_connections.begin();
+       it != g_state.active_connections.end(); ++it) {
+    auto &connection = *it;
 
-        ssize_t bytes_read = recv(connection->socket_fd, connection->buffer + connection->buffer_pos
-                , sizeof(connection->buffer) - connection->buffer_pos, 0);
+    ssize_t bytes_read =
+        recv(connection->socket_fd, connection->buffer + connection->buffer_pos,
+             sizeof(connection->buffer) - connection->buffer_pos, 0);
 
-        if(bytes_read == -1)
-        {
-            if((errno == EAGAIN) || (errno == EWOULDBLOCK))
-            {
-                continue;
-            }
-            perror("lost connection");
-            it = g_state.active_connections.erase(it);
-            if(it == g_state.active_connections.end())
-            {
-                break;
-            }
-        }
-
-        connection->buffer_pos += bytes_read;
-
-        if(connection->buffer_pos < 4)
-        {
-            continue;
-        }
-
-        auto packet_size = *reinterpret_cast<const uint16_t*>(connection->buffer + 2);
-
-        while(true)
-        {
-            if(connection->buffer_pos >= packet_size)
-            {
-                auto packet = std::make_shared<ConnectionBuffer>();
-                packet->connection = connection.get();
-                memcpy(packet->buffer, connection->buffer, packet_size);
-                g_state.completed_packets.push(std::move(packet));
-            }
-
-            const auto delta = connection->buffer_pos - packet_size;
-
-            if(delta == 0)
-            {
-                connection->buffer_pos = 0;
-                break;
-            }
-
-            memmove(connection->buffer, connection->buffer + connection->buffer_pos, delta);
-            connection->buffer_pos = delta;
-
-            packet_size = *reinterpret_cast<const uint16_t*>(connection->buffer + 2);
-
-            if(delta < packet_size)
-            {
-                break;
-            }
-        }
+    if (bytes_read == -1) {
+      if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+        continue;
+      }
+      perror("lost connection");
+      it = g_state.active_connections.erase(it);
+      if (it == g_state.active_connections.end()) {
+        break;
+      }
     }
+
+    connection->buffer_pos += bytes_read;
+
+    if (connection->buffer_pos < 4) {
+      continue;
+    }
+
+    auto packet_size =
+        *reinterpret_cast<const uint16_t *>(connection->buffer + 2);
+
+    while (true) {
+      if (connection->buffer_pos >= packet_size) {
+        auto packet = std::make_shared<ConnectionBuffer>();
+        packet->connection = connection.get();
+        memcpy(packet->buffer, connection->buffer, packet_size);
+        g_state.completed_packets.push(std::move(packet));
+
+        connection->buffer_pos = 0;
+      }
+
+      auto delta = connection->buffer_pos - packet_size;
+
+      if (delta == 0) {
+        connection->buffer_pos = 0;
+        break;
+      }
+
+      memmove(connection->buffer, connection->buffer + connection->buffer_pos,
+              delta);
+      connection->buffer_pos = delta;
+
+      packet_size = *reinterpret_cast<const uint16_t *>(connection->buffer + 2);
+
+      if (delta < packet_size) {
+        break;
+      }
+    }
+  }
 }
 
-void handle_pending_connections()
-{
-    std::lock_guard guard{g_state.pending_connections_mtx};
+void handle_pending_connections() {
+  std::lock_guard guard{g_state.pending_connections_mtx};
 
-    while(!g_state.pending_connections.empty())
-    {
-        auto& conn = g_state.pending_connections.top();
+  while (!g_state.pending_connections.empty()) {
+    auto &conn = g_state.pending_connections.top();
 
-        g_state.active_connections.emplace_back(std::move(conn));
+    g_state.active_connections.emplace_back(std::move(conn));
 
-        g_state.pending_connections.pop();
-    }
+    g_state.pending_connections.pop();
+  }
 }
 
-void server_thread()
-{
-    for(; g_state.b_should_run.load(std::memory_order::relaxed);)
-    {
-        //Needs to be ran on a sepparate thread because of while(true)
-        recieve_all_connections();
-        handle_packets();
-        handle_pending_connections();
-    }
+void server_thread() {
+  for (; g_state.b_should_run.load(std::memory_order::relaxed);) {
+    // Needs to be ran on a sepparate thread because of while(true)
+    recieve_all_connections();
+    handle_packets();
+    handle_pending_connections();
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  }
 }
 
-void TerminationRequestHandler(int32_t signal)
-{
-    if(g_state.b_should_run.exchange(false))
-    {
-        const auto s = g_state.server_socket_fd.exchange(-1);
-        if(s > 0)
-        {
-            close(s);
-        }
+void TerminationRequestHandler(int32_t signal) {
+  if (g_state.b_should_run.exchange(false)) {
+    const auto s = g_state.server_socket_fd.exchange(-1);
+    if (s > 0) {
+      close(s);
+      exit(EXIT_SUCCESS);
     }
+  }
 }
 
-int main()
-{
-    (void)::signal(SIGINT, &TerminationRequestHandler);  //Interrupt signal (CTRL + C)
-    (void)::signal(SIGTERM, &TerminationRequestHandler); //Termination request
+int main() {
+  // TODO: Fix ctrl + c
+  (void)::signal(SIGINT,
+                 &TerminationRequestHandler); // Interrupt signal (CTRL + C)
+  (void)::signal(SIGTERM, &TerminationRequestHandler); // Termination request
+  (void)::signal(SIGKILL, &TerminationRequestHandler);
 
-    ClientController::initialize();
+  ClientController::initialize();
 
-    g_state.thread_pool.start(6);
-    auto accept_thread = std::jthread(accept_thread_handler);
+  g_state.thread_pool.start(6);
+  auto accept_thread = std::jthread(accept_thread_handler);
 
-    server_thread();
+  server_thread();
 }
