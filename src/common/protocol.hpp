02561104554a51fb7830e3e8ae108d62634cd10b
PaulDotSH <84643540+PaulDotSH@users.noreply.github.com>
2024-04-24 18:58:53 -0400

    more stuff

--- src/common/protocol.hpp
+++ src/common/protocol.hpp
@@ -1,56 +1,66 @@
 #pragma once
 
+#include <cerrno>
 #include <cstdint>
+#include <cstdio>
+#include <sys/socket.h>
+#include <thread>
 
-enum ClientRequestPacket : uint8_t //Ensure this is one byte
+enum class PacketType : uint8_t // Ensure this is one byte
 {
-    REQ_FILES_TRANSFER_START
-    , REQ_FILE_TRANSFER_CHUNK
-    , REQ_FILE_TRANSFER_END
-    , REQ_CRC_VERIFY
-    , REQ_SERVER_STATUS
-    , REQ_GET_SETTINGS
-    , REQ_SET_SETTING
+  REQ_FILES_TRANSFER_START,
+  REQ_FILE_TRANSFER_CHUNK,
+  REQ_FILE_TRANSFER_END,
+  REQ_CRC_VERIFY,
+  REQ_SERVER_STATUS,
+  REQ_GET_SETTINGS,
+  REQ_SET_SETTING,
+  RESP_OK,
+  RESP_CRC_FAILED,
+  RESP_SERVER_STATUS_RESPONSE
 };
 
-enum ServerResponse : uint8_t
-{
-    RESP_OK
-    , RESP_CRC_FAILED
-    , RESP_SERVER_STATUS_RESPONSE
+struct PacketHeader {
+  uint8_t version;
+  PacketType command;
+  uint16_t total_size;
 };
 
-struct ServerProtocol
-{
-    uint8_t             version;
-    ClientRequestPacket req_type;
-    uint16_t            total_size;
-    uint8_t             buffer[UINT16_MAX - sizeof(uint16_t) - sizeof(uint8_t) - sizeof(ClientRequestPacket)];
+struct Packet {
+  PacketHeader header;
+  uint8_t buffer[UINT16_MAX - sizeof(PacketHeader)];
 };
 
-struct ClientProtocol
-{
-    uint8_t        version;
-    ServerResponse resp_type;
-    uint16_t       total_size;
-    uint8_t        buffer[UINT16_MAX - sizeof(uint16_t) - sizeof(uint8_t) - sizeof(ClientRequestPacket)];
-};
+struct ConnectionWrapper {
+  uint32_t socket_fd;
+  uint8_t buffer[UINT16_MAX];
+  uint16_t buffer_pos;
+  bool is_admin;
+  uint32_t id; // Just in case
 
-struct ConnectionWrapper
-{
-    uint32_t socket_fd;
-    uint8_t  buffer[UINT16_MAX];
-    uint16_t buffer_pos;
-    bool     is_admin;
-    uint32_t id; //Just in case
-};
-
-struct ConnectionBuffer
-{
-    ClientRequestPacket get_packet_type()
-    {
-        return (ClientRequestPacket)connection->buffer[1];
+  void send_response_sync(uint8_t *buffer, size_t length) {
+    int sent_bytes = 0;
+    while (true) {
+      const int result =
+          send(this->socket_fd, buffer + sent_bytes, length - sent_bytes, 0);
+      if (result == -1) {
+        if (errno == EWOULDBLOCK || errno == EAGAIN) {
+          std::this_thread::sleep_for(std::chrono::milliseconds(1));
+          continue;
+        }
+        perror("send");
+        return;
+      }
+      sent_bytes += result;
+      if (sent_bytes >= length) {
+        return;
+      }
     }
-    ConnectionWrapper* connection;
-    uint8_t            buffer[UINT16_MAX];
+  }
+};
+
+struct ConnectionBuffer {
+  PacketType get_packet_type() { return (PacketType)connection->buffer[1]; }
+  ConnectionWrapper *connection;
+  uint8_t buffer[UINT16_MAX];
 };
