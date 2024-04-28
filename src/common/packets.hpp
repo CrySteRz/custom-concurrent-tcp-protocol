1b3c09ca963753989d637fe028919ce242eb1d78
pauldotsh <paulandreiabrudan@proton.me>
2024-04-28 14:30:24 -0400

    small refactor for the client, need to modularize code

--- src/common/packets.hpp
+++ src/common/packets.hpp
@@ -2,9 +2,24 @@
 #include "../server/server_info.hpp"
 #include "protocol.hpp"
 
-struct ServerStatusPacket {
-  PacketHeader header;
-  MemoryInfo memory_info;
-  int8_t cpu_usage;
-  long uptime_seconds;
+struct ServerStatusPacket
+{
+    PacketHeader header;
+    MemoryInfo   memory_info;
+    int8_t       cpu_usage;
+    long         uptime_seconds;
+};
+
+struct FileInfo
+{
+    char     file_name[32];
+    uint16_t file_size;
+    uint8_t  hash[16];
+};
+
+struct FilesStartPacket
+{
+    PacketHeader header;
+    uint8_t      file_count;
+    FileInfo     file_info;
 };
